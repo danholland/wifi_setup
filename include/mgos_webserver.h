c8bd0962e1a69b3650ae06f76cc9447a9c805444
Holland <Dan.Holland@ricardo.com>
2019-06-12 11:00:34 +0100

    ...

---
+++ include/mgos_webserver.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <mgos.h>
+#include "mgos_http_server.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+  /**
+ * Start captive portal
+ */
+  bool webserver_start(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
